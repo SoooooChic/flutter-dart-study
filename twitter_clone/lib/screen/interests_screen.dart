f6edafd85dc5ab756b6ec179522ecd37e966a129
SoooooChic <ksunny7594@gmail.com>
2025-01-25 12:35:40 +0900

    assginment12-edit

--- twitter_clone/lib/screen/interests_screen.dart
+++ twitter_clone/lib/screen/interests_screen.dart
@@ -1,23 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:twitter_clone/screen/interests_part_two_screen.dart';
 import '../constants/gaps.dart';
 import '../constants/sizes.dart';
 import '../widgets/form_button.dart';
-// import '../widgets/interest_button.dart';
 
 const Map<String, Map<String, List>> interests = {
   "part1": {
     "data": [
-      "Fashion & beauty",
-      "Outdoors",
-      "Arts & culture",
-      "Animation \n& comics",
-      "Business \n& finance",
-      "Food",
-      "Travel",
-      "Entertainment",
-      "Music",
-      "Gaming",
       "Fashion & beauty",
       "Outdoors",
       "Arts & culture",
@@ -65,8 +55,6 @@ class InterestsScreen extends StatefulWidget {
 }
 
 class _InterestsScreenState extends State<InterestsScreen> {
-  // final ScrollController _scrollController = ScrollController();
-
   @override
   void initState() {
     super.initState();
@@ -78,11 +66,6 @@ class _InterestsScreenState extends State<InterestsScreen> {
 
   late List<bool> _isSelected;
 
-  // //final
-  // final
-  // List<bool> _isSelected =
-  //     List.generate(interests['part1']!['data']!.length, (index) => false);
-
   void _onTap(int index) {
     if (_interestCount == 3 && !_isSelected[index] == true) return;
     _isSelected[index] = !_isSelected[index];
@@ -90,14 +73,22 @@ class _InterestsScreenState extends State<InterestsScreen> {
     setState(() {});
   }
 
+  void _onInterestPartTwoTap() {
+    if (_interestCount == 3) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => InterestsPartTwoScreen(),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Scrollbar(
-          //controller: _scrollController,
           child: SingleChildScrollView(
-            // controller: _scrollController,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: Sizes.size40),
               child: Column(
@@ -160,11 +151,9 @@ class _InterestsScreenState extends State<InterestsScreen> {
                   Container(
                     alignment: Alignment.center,
                     child: Wrap(
-                      direction: Axis.horizontal, // 가로 방향
-                      alignment: WrapAlignment.start, // 가로 정렬
-                      // 세로 간격
+                      direction: Axis.horizontal,
+                      alignment: WrapAlignment.start,
                       runSpacing: 10,
-                      // 가로 간격
                       spacing: 10,
                       children: [
                         for (int i = 0; i < data.length; i++)
@@ -228,33 +217,6 @@ class _InterestsScreenState extends State<InterestsScreen> {
                               ),
                             ),
                           ),
-
-                        // for (int i = 0; i < data.length; i++)
-                        //     // print("Index: $i, Value: ${data[i]}");
-                        //     InterestButton(
-                        //       interest: data[i],
-                        //       onTap: () {
-                        //         // 외부에서 상태를 변경
-                        //         // _controllers[index].value = !_controllers[index].value;
-                        //         setState(() {
-                        //           _isSelected[i] = !_isSelected[i];
-                        //           _cnt += (_isSelected[i]) ? 1 : -1;
-                        //         }); // 선택된 개수 업데이트
-                        //       },
-                        //     ),
-                        //
-                        // for (var interest in interests['part1']!['data']!)
-                        //   InterestButton(
-                        //     interest: interest,
-                        //     onTap: () {
-                        //       // 외부에서 상태를 변경
-                        //       // _controllers[index].value = !_controllers[index].value;
-                        //       setState(() {
-                        //         _isSelected[] = !_isSelected;
-                        //         _cnt += (_isSelected) ? 1 : -1;
-                        //       }); // 선택된 개수 업데이트
-                        //     },
-                        //   ),
                       ],
                     ),
                   ),
@@ -283,7 +245,7 @@ class _InterestsScreenState extends State<InterestsScreen> {
             SizedBox(
               width: 150,
               child: GestureDetector(
-                onTap: () {}, // interest part two
+                onTap: _onInterestPartTwoTap, // interest part two
                 child: FormButton(
                   disabled: _interestCount == 3 ? true : false,
                   buttonSize: 0.5,
