9147c1caf2745b6cda318e2012b6058a813fb3b8
SoooooChic <ksunny7594@gmail.com>
2025-02-21 18:09:51 +0900

    assignment22 - edit

--- threads_clone/lib/views/profile_screen.dart
+++ threads_clone/lib/views/profile_screen.dart
@@ -47,194 +47,196 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     // final isDark = isDarkMode(context);
 
-    return DefaultTabController(
-      length: 2,
-      child: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return [
-            SliverAppBar(
-              title: IconButton(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.language,
-                  size: Sizes.size20,
-                ),
-              ),
-              actions: [
-                IconButton(
+    return SafeArea(
+      child: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (context, innerBoxIsScrolled) {
+            return [
+              SliverAppBar(
+                title: IconButton(
                   onPressed: () {},
-                  icon: const Icon(
-                    FontAwesomeIcons.instagram,
-                    size: Sizes.size24,
-                  ),
-                ),
-                IconButton(
-                  onPressed: () => _onGearPressed(context),
-                  icon: const Icon(
-                    Icons.menu,
+                  icon: Icon(
+                    Icons.language,
                     size: Sizes.size20,
                   ),
-                )
-              ],
-            ),
-            SliverToBoxAdapter(
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: Sizes.size18,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Jane',
-                              style: TextStyle(
-                                fontSize: Sizes.size18,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            Gaps.v5,
-                            Text('jane_mobbin threads.net'),
-                          ],
-                        ),
-                        CircleAvatar(
-                          radius: 30,
-                          foregroundImage: NetworkImage(avatarUrls[0]),
-                        ),
-                      ],
+                actions: [
+                  IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      FontAwesomeIcons.instagram,
+                      size: Sizes.size24,
                     ),
-                    Gaps.v5,
-                    Text('Plant enthusiast!'),
-                    Gaps.v10,
-                    Row(
-                      children: [
-                        Stack(
-                          clipBehavior: Clip.none,
-                          children: [
-                            CircleAvatar(
-                              foregroundImage: NetworkImage(avatarUrls[1]),
-                              radius: 9,
-                            ),
-                            Positioned(
-                              right: -15,
-                              child: CircleAvatar(
-                                foregroundImage: NetworkImage(avatarUrls[2]),
+                  ),
+                  IconButton(
+                    onPressed: () => _onGearPressed(context),
+                    icon: const Icon(
+                      Icons.menu,
+                      size: Sizes.size20,
+                    ),
+                  )
+                ],
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: Sizes.size18,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Jane',
+                                style: TextStyle(
+                                  fontSize: Sizes.size18,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Gaps.v5,
+                              Text('jane_mobbin threads.net'),
+                            ],
+                          ),
+                          CircleAvatar(
+                            radius: 30,
+                            foregroundImage: NetworkImage(avatarUrls[0]),
+                          ),
+                        ],
+                      ),
+                      Gaps.v5,
+                      Text('Plant enthusiast!'),
+                      Gaps.v10,
+                      Row(
+                        children: [
+                          Stack(
+                            clipBehavior: Clip.none,
+                            children: [
+                              CircleAvatar(
+                                foregroundImage: NetworkImage(avatarUrls[1]),
                                 radius: 9,
                               ),
-                            ),
-                          ],
-                        ),
-                        Gaps.h24,
-                        Text(
-                          '2 followers',
-                          style: TextStyle(
-                            color: Colors.grey,
+                              Positioned(
+                                right: -15,
+                                child: CircleAvatar(
+                                  foregroundImage: NetworkImage(avatarUrls[2]),
+                                  radius: 9,
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
-                    ),
-                    Gaps.v14,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Expanded(
-                          child: Container(
-                            padding: EdgeInsets.symmetric(vertical: 6),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey.shade500),
-                              borderRadius: BorderRadius.circular(8),
+                          Gaps.h24,
+                          Text(
+                            '2 followers',
+                            style: TextStyle(
+                              color: Colors.grey,
                             ),
-                            child: Text(
-                              "Edit profile",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                // color: Colors.black,
+                          ),
+                        ],
+                      ),
+                      Gaps.v14,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 6),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey.shade500),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Text(
+                                "Edit profile",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  // color: Colors.black,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        SizedBox(width: 10),
-                        Expanded(
-                          child: Container(
-                            padding: EdgeInsets.symmetric(vertical: 6),
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.grey),
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Text(
-                              "Share profile",
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                // color: Colors.black,
+                          SizedBox(width: 10),
+                          Expanded(
+                            child: Container(
+                              padding: EdgeInsets.symmetric(vertical: 6),
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.grey),
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Text(
+                                "Share profile",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  // color: Colors.black,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Gaps.v10,
-                  ],
+                        ],
+                      ),
+                      Gaps.v10,
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SliverPersistentHeader(
-              delegate: PersistentTabBar(),
-              pinned: true,
-            ),
-          ];
-        },
-        body: TabBarView(
-          children: [
-            Container(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              child: ListView.builder(
-                padding: EdgeInsets.only(top: 20),
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: const [
-                      Thread(),
-                      Gaps.v16,
-                      Divider(
-                        height: 0,
-                        thickness: 1,
-                      ),
-                      Gaps.v16,
-                    ],
-                  );
-                },
+              SliverPersistentHeader(
+                delegate: PersistentTabBar(),
+                pinned: true,
               ),
-            ),
-            Container(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              child: ListView.builder(
-                padding: EdgeInsets.only(top: 20),
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: const [
-                      Thread(),
-                      Gaps.v16,
-                      Divider(
-                        height: 0,
-                        thickness: 1,
-                      ),
-                      Gaps.v16,
-                    ],
-                  );
-                },
+            ];
+          },
+          body: TabBarView(
+            children: [
+              Container(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 20),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: const [
+                        Thread(),
+                        Gaps.v16,
+                        Divider(
+                          height: 0,
+                          thickness: 1,
+                        ),
+                        Gaps.v16,
+                      ],
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+              Container(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 20),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      children: const [
+                        Thread(),
+                        Gaps.v16,
+                        Divider(
+                          height: 0,
+                          thickness: 1,
+                        ),
+                        Gaps.v16,
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
