4406947bc7c2578a2c0730d0922ad9c600ffdb98
SoooooChic <ksunny7594@gmail.com>
2025-03-13 15:50:36 +0900

    Animations - #5 Music Player Project

--- animations/lib/screens/menu_screen.dart
+++ animations/lib/screens/menu_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animations/screens/apple_watch_screen.dart';
 import 'package:animations/screens/explicit_animations_screen.dart';
 import 'package:animations/screens/implicit_animations_dscreen.dart';
+import 'package:animations/screens/music_player_screen.dart';
 import 'package:animations/screens/swiping_cards_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -41,6 +42,12 @@ class MenuScreen extends StatelessWidget {
                 _goToPage(context, const SwipingCardsScreen());
               },
               child: const Text('Swiping Cards'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(context, const MusicPlayerScreen());
+              },
+              child: const Text('Music Player'),
             ),
           ],
         ),
